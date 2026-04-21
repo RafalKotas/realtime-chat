2a27c8d8f2ae454838bd08011b88cd387a4aa7fc
Raffel222 <Raffel222@gmail.com>
2026-04-21 18:02:25 +0200

    test(security): configure test security and context loading

--- backend/src/test/java/com/rafkot/chatapp/ChatappApplicationTests.java
+++ backend/src/test/java/com/rafkot/chatapp/ChatappApplicationTests.java
@@ -1,10 +1,31 @@
 package com.rafkot.chatapp;
 
+import com.rafkot.chatapp.security.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import({TestSecurityConfig.class})
 class ChatappApplicationTests {
+
+	@MockitoBean
+	private JwtService jwtService;
+
+	@MockitoBean
+	private JwtEncoder jwtEncoder;
+
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
+
+	@MockitoBean
+	private PasswordEncoder passwordEncoder;
 
 	@Test
 	void contextLoads() {
