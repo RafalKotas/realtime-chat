ff0190c03db26e0389a91d9d0e37c369712cc507
Raffel222 <Raffel222@gmail.com>
2026-04-21 18:00:08 +0200

    feat(auth): add authentication endpoint

---
+++ backend/src/main/java/com/rafkot/chatapp/security/AuthController.java
@@ -0,0 +1,29 @@
+package com.rafkot.chatapp.security;
+
+import com.rafkot.chatapp.AuthenticationService;
+import com.rafkot.chatapp.security.dto.AuthenticationRequestDto;
+import com.rafkot.chatapp.security.dto.AuthenticationResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDto> authenticate(
+            @RequestBody final AuthenticationRequestDto authenticationRequestDto
+    ) {
+        return ResponseEntity.ok(
+                authenticationService.authenticate(authenticationRequestDto)
+        );
+    }
+
+}
