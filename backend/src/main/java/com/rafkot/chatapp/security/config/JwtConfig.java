f05b8b1bb8fc795c917e68a3938d99ba2dfa5e7d
Raffel222 <Raffel222@gmail.com>
2026-04-21 17:34:21 +0200

    feat(security): add JWT configuration and security setup

---
+++ backend/src/main/java/com/rafkot/chatapp/security/config/JwtConfig.java
@@ -0,0 +1,96 @@
+package com.rafkot.chatapp.security.config;
+
+import com.rafkot.chatapp.security.JwtService;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.time.Duration;
+import java.util.Base64;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "jwt")
+public class JwtConfig {
+
+    private Resource privateKey;
+
+    private Resource publicKey;
+
+    private Duration ttl;
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        final var jwk = new RSAKey.Builder(getPublicKey())
+                .privateKey(getPrivateKey())
+                .build();
+
+        return new NimbusJwtEncoder(
+                new ImmutableJWKSet<>(new JWKSet(jwk))
+        );
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(getPublicKey()).build();
+    }
+
+    @Bean
+    public JwtService jwtService(
+            @Value("${spring.application.name}") final String appName,
+            final JwtEncoder jwtEncoder
+    ) {
+        return new JwtService(appName, ttl, jwtEncoder);
+    }
+
+    private RSAPublicKey getPublicKey() {
+        try (InputStream is = publicKey.getInputStream()) {
+            String key = new String(is.readAllBytes())
+                    .replace("-----BEGIN PUBLIC KEY-----", "")
+                    .replace("-----END PUBLIC KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] decoded = Base64.getDecoder().decode(key);
+            X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
+
+            return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(spec);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot load public key", e);
+        }
+    }
+
+    private RSAPrivateKey getPrivateKey() {
+        try (InputStream is = privateKey.getInputStream()) {
+            String key = new String(is.readAllBytes())
+                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                    .replace("-----END PRIVATE KEY-----", "")
+                    .replaceAll("\\s", "");
+
+            byte[] decoded = Base64.getDecoder().decode(key);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+
+            return (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(spec);
+        } catch (Exception e) {
+            throw new IllegalStateException("Cannot load private key", e);
+        }
+    }
+}
---
+++ backend/src/main/java/com/rafkot/chatapp/security/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.rafkot.chatapp.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Profile("!test")
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            final HttpSecurity http
+    ) {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/user/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(server -> server
+                        .jwt(Customizer.withDefaults())
+                        .authenticationEntryPoint(
+                                new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(
+                                new BearerTokenAccessDeniedHandler())
+                        )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) {
+        return config.getAuthenticationManager();
+    }
+}
