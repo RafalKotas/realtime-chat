7d4dd6a5afdab8435570b7093478df57552dbf76
Raffel222 <Raffel222@gmail.com>
2026-04-25 01:10:57 +0200

    feat: add UserMapper component for mapping User to DTO

---
+++ backend/src/main/java/com/rafkot/chatapp/UserMapper.java
@@ -0,0 +1,15 @@
+package com.rafkot.chatapp;
+
+import com.rafkot.chatapp.user.User;
+import com.rafkot.chatapp.user.dto.UserProfileDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserProfileDto toUserProfileDto(User user) {
+        return new UserProfileDto(
+                user.getUsername(),
+                user.getEmail()
+        );
+    }
+}
