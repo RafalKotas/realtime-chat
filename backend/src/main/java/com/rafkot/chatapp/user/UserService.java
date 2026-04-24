56e936451a489f4a87697489ff48fb081a51342c
Raffel222 <Raffel222@gmail.com>
2026-04-25 01:10:40 +0200

    refactor: remove Lombok from UserService and extract UserMapper

--- backend/src/main/java/com/rafkot/chatapp/user/UserService.java
+++ backend/src/main/java/com/rafkot/chatapp/user/UserService.java
@@ -1,29 +1,22 @@
 package com.rafkot.chatapp.user;
 
-import com.rafkot.chatapp.user.dto.UserProfileDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import static org.springframework.http.HttpStatus.GONE;
 
 @Service
-@RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User getUserByUsername(final String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(GONE,
                         "The user account has been deleted or inactivated"));
-    }
-
-    @Component
-    public class UserMapper {
-        public UserProfileDto toUserProfileDto(final User user) {
-            return new UserProfileDto(user.getEmail(), user.getUsername());
-        }
     }
 }
