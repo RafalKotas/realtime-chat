b8e35d4b6f3fad0bcbd613a72fd8f5843e8a1876
Raffel222 <Raffel222@gmail.com>
2026-05-08 20:18:40 +0200

    tests: MessageMapperTest

---
+++ backend/src/test/java/com/rafkot/chatapp/message/MessageMapperTest.java
@@ -0,0 +1,50 @@
+package com.rafkot.chatapp.message;
+
+import com.rafkot.chatapp.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MessageMapperTest {
+
+    MessageMapper subject;
+
+    @BeforeEach
+    void setUp() {
+        subject = new MessageMapper();
+    }
+
+    @Test
+    void shouldMapMessageToMessageResponseDto() {
+        // given
+        User testSender = new User("testuser1", "testuser1@mail.com", "testpass1");
+        User testRecipient = new User("testuser2", "testuser2@mail.com", "testpass2");
+
+        String content = "Hello World!";
+        Message testMessage = new Message(testSender, testRecipient, content);
+
+        Instant now  = Instant.now();
+        testMessage.setCreatedDate(now);
+        testMessage.setModifiedDate(now);
+
+        UUID uuid = UUID.fromString("11111111-2222-3333-4444-555555555555");
+        testMessage.setId(uuid);
+
+        // when
+        MessageResponseDto result = subject.mapMessageToDto(testMessage);
+
+        // then
+        assertThat(result).isNotNull();
+
+        assertThat(result.content()).isNotNull().isEqualTo("Hello World!");
+        assertThat(result.createdDate()).isNotNull().isEqualTo(now);
+        assertThat(result.modifiedDate()).isNotNull().isEqualTo(now);
+        assertThat(result.senderUsername()).isNotNull().isEqualTo("testuser1");
+        assertThat(result.receiverUsername()).isNotNull().isEqualTo("testuser2");
+        assertThat(result.messageId()).isNotNull().isEqualTo(UUID.fromString("11111111-2222-3333-4444-555555555555"));
+    }
+}
