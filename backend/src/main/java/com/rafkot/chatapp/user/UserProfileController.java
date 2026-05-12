6e0df20df17c9ecf2cfbc1ed7852a277df0b8f83
Raffel222 <Raffel222@gmail.com>
2026-05-12 21:57:14 +0200

    feature: add change-password endpoint, exception handling and config

--- backend/src/main/java/com/rafkot/chatapp/user/UserProfileController.java
+++ backend/src/main/java/com/rafkot/chatapp/user/UserProfileController.java
@@ -1,21 +1,24 @@
 package com.rafkot.chatapp.user;
 
+import com.rafkot.chatapp.auth.dto.ChangePasswordRequestDto;
+import com.rafkot.chatapp.auth.dto.ChangePasswordResponseDto;
 import com.rafkot.chatapp.user.dto.UserProfileDto;
 import com.rafkot.chatapp.user.exception.UserValidationException;
 import com.rafkot.chatapp.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserProfileController {
 
     private final UserService userService;
@@ -23,14 +26,33 @@ public class UserProfileController {
 
     @GetMapping("/me")
     public ResponseEntity<UserProfileDto> getUserProfile(
-            @AuthenticationPrincipal(expression = "username") String username) {
-
-        if (username == null) {
-            throw new UserValidationException(HttpStatus.UNAUTHORIZED, Map.of("authentication", "username is null"));
+            @AuthenticationPrincipal Jwt jwt) {
+        if (jwt == null || jwt.getClaim("sub") == null) {
+            throw new UserValidationException(HttpStatus.UNAUTHORIZED,
+                    Map.of("authentication", "username is null"));
         }
+
+        String username = jwt.getClaim("sub");
 
         final User user = userService.getUserByUsername(username);
 
         return ResponseEntity.ok(userMapper.toUserProfileDto(user));
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<ChangePasswordResponseDto> changePassword(
+            @AuthenticationPrincipal Jwt jwt,
+            @RequestBody ChangePasswordRequestDto dto
+    ) {
+        if (jwt == null || jwt.getClaim("sub") == null) {
+            throw new UserValidationException(HttpStatus.UNAUTHORIZED,
+                    Map.of("authentication", "username is null"));
+        }
+
+        String username = jwt.getClaim("sub");
+
+        userService.changePassword(username, dto.password(), dto.confirmPassword());
+
+        return ResponseEntity.ok(new ChangePasswordResponseDto("Password changed correctly."));
     }
 }
