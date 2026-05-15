383b7b290190f91c96819346485ad21a6cc5f923
Raffel222 <Raffel222@gmail.com>
2026-05-15 16:12:09 +0200

    feat: GET messages between users pair

--- backend/src/main/java/com/rafkot/chatapp/message/MessageController.java
+++ backend/src/main/java/com/rafkot/chatapp/message/MessageController.java
@@ -1,20 +1,20 @@
 package com.rafkot.chatapp.message;
 
 import com.rafkot.chatapp.user.exception.UserValidationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/message")
+@Slf4j
 public class MessageController {
 
     MessageService messageService;
@@ -36,5 +36,21 @@ public class MessageController {
         Map<String, List<MessageResponseDto>> userGroupedMessages = messageService.getGroupedUserChatsMessages(uuid);
 
         return ResponseEntity.ok(userGroupedMessages);
+    }
+
+    @GetMapping("/{userId}/{partnerUsername}")
+    public ResponseEntity<List<MessageResponseDto>> getMessagesWithGivenUserId(
+            @AuthenticationPrincipal Jwt jwt, @PathVariable UUID userId, @PathVariable String partnerUsername) {
+
+        log.info("Fetching messages for user {} with partner {}", userId, partnerUsername);
+        String username = jwt.getClaim("sub");
+
+        if (username == null) {
+            throw new UserValidationException(HttpStatus.UNAUTHORIZED, Map.of("authentication", "username is null"));
+        }
+
+        List<MessageResponseDto> messages = messageService.getMessagesBetweenUsers(userId, partnerUsername);
+
+        return ResponseEntity.ok(messages);
     }
 }
