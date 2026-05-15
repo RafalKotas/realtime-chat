7b2620c040e20c8d95e82668173983cfe4379df3
Raffel222 <Raffel222@gmail.com>
2026-05-15 18:28:25 +0200

    fix: tests - ActiveProfiles and TestSecurityConfig

--- backend/src/test/java/com/rafkot/chatapp/common/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/com/rafkot/chatapp/common/GlobalExceptionHandlerTest.java
@@ -1,18 +1,23 @@
 package com.rafkot.chatapp.common;
 
+import com.rafkot.chatapp.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@Import({TestSecurityConfig.class})
 @SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class GlobalExceptionHandlerTest {
 
     @Autowired
--- backend/src/test/java/com/rafkot/chatapp/config/SecurityBeansTest.java
+++ backend/src/test/java/com/rafkot/chatapp/config/SecurityBeansTest.java
@@ -3,11 +3,15 @@ package com.rafkot.chatapp.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Import({TestSecurityConfig.class})
+@ActiveProfiles("test")
 @SpringBootTest
 class SecurityBeansTest {
 
