27892ea59c559a69cc158f79b343ebcca42018d4
Raffel222 <Raffel222@gmail.com>
2026-05-15 16:17:12 +0200

    chore: extract ContactTab component content - ContactsSidebar

--- frontend/src/usercontent/ContactsSidebar.tsx
+++ frontend/src/usercontent/ContactsSidebar.tsx
@@ -4,32 +4,21 @@ import {
     SidebarHeader, 
     SidebarContent,
     SidebarFooter, 
-    SidebarMenu, 
-    SidebarMenuItem
+    SidebarMenu
 } from '@/components/ui/sidebar'
+
 import {
-} from '@/components/ui/dialog'
-import { 
-  Avatar, 
-  AvatarFallback, 
-  AvatarImage 
-} from '@/components/ui/avatar'
-import { 
-  Tooltip, 
-  TooltipContent, 
-  TooltipProvider, 
-  TooltipTrigger
+  TooltipProvider
 } from '@/components/ui/tooltip'
 import { InputGroup, InputGroupInput, InputGroupAddon } from '@/components/ui/input-group'
 import { IconFilter } from '@tabler/icons-react'
 import { useMessagingStore} from '@/messaging/messaging-store'
-import type { Message } from '@/messaging/messaging-store'
 import ContactFinder from './ContactFinder'
-import { messageTimeCaption } from '@/messaging/dateUtils'
+import ContactTab from './ContactTab'
 
 const ContactsSidebar = () => {
 
-  const {currentChatReceiverUsername, userChats, setCurrentChatReceiverUsername, getLastMessage, filterUserChats} = useMessagingStore()
+  const {userChats, filterUserChats} = useMessagingStore()
   
   const [searchValue, setSearchValue] = useState<string>("")
   const [open, setOpen] = useState<boolean>(false)
@@ -42,18 +31,6 @@ const ContactsSidebar = () => {
     setSearchValue(event.target.value)
   }
 
-  const imageFallback = (name: string) => {
-    return name && name.length > 0 ? name.charAt(0) + name.charAt(name.length - 1) : "?"
-  }
-
-  const getLastMessageContent = (contactUsername: string, message: Message | null) => {
-    if (!message) return "No messages yet"
-    const shortenedMessage = message.content.length > 20 ? message.content.substring(0, 15) + "..." : message.content
-    return <>{message.senderUsername === contactUsername ? "" : <b>You: </b>}<span>{shortenedMessage}</span></>
-  }
-  
-
-  const contactTabBasicStyles = "flex flex-row gap-2 p-2 mb-2 items-center cursor-pointer rounded-md bg-gray-200 hover:bg-lime-300 hover:border-2 hover:border-gray-400 hover:mb-1" 
 
   return (
     <Sidebar className="pt-10">
@@ -73,32 +50,7 @@ const ContactsSidebar = () => {
         <SidebarContent>
           <SidebarMenu className="flex flex-col gap-2 p-4 overflow-y-auto z-0">
               {Object.keys(filteredUserChats).map((contactUsername) => (
-                <Tooltip>
-                  <TooltipTrigger>
-                    <SidebarMenuItem 
-                      key={contactUsername} 
-                      className={contactTabBasicStyles + (currentChatReceiverUsername === contactUsername ? " cursor-default hover:bg-green-600 bg-green-600 border-2 border-gray-400" : "")}
-                      onClick={() => {
-                        setCurrentChatReceiverUsername(contactUsername)
-                      }}
-                    >
-                      <Avatar>
-                        <AvatarImage src='https://github.com/shadcn.png' />
-                        <AvatarFallback>{imageFallback(contactUsername)}</AvatarFallback>
-                      </Avatar>
-                      <div className="flex flex-col">
-                          <header className="text-sm font-bold text-gray-800">{contactUsername}</header>
-                          <p className="text-sm text-gray-800">
-                            {getLastMessageContent(contactUsername, getLastMessage(contactUsername))}
-                          </p>
-                          <p className="text-xs text-gray-800">{messageTimeCaption(getLastMessage(contactUsername)?.createdDate || "")}</p>
-                      </div>
-                    </SidebarMenuItem>
-                  </TooltipTrigger>
-                  <TooltipContent hidden={currentChatReceiverUsername === contactUsername} side="right">
-                    <p>Open chat!</p>
-                  </TooltipContent>
-                </Tooltip>
+                <ContactTab key={contactUsername} contactUsername={contactUsername} />
               ))}
           </SidebarMenu>
         </SidebarContent>
